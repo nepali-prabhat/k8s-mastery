ae94cb3aac4b4946016480dcc780667ae86a7050
Pravat Pandey <pandeypravat@gmail.com>
2025-09-23 00:53:59 -0400

    added http://

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch(process.env.NODE_ENV==="production"? "34.130.129.106:8080/sentiment" : 'http://localhost:8080/sentiment', {
+        fetch(process.env.NODE_ENV==="production"? "http://34.130.129.106:8080/sentiment" : 'http://localhost:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
